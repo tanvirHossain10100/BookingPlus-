f0c9bb37f428516833810c25b095c11fd2998518
tanvirHossain10100 <tanvirhossan528@gmail.com>
2024-01-08 12:52:48 +0600

    add form validation for signup page using react form hook

--- src/pages/Signup/Signup.jsx
+++ src/pages/Signup/Signup.jsx
@@ -7,8 +7,12 @@ export const Signup = () => {
     watch,
     formState: { errors },
   } = useForm();
-  console.log(handleSubmit);
-  const onSubmit = (data) => console.log(data);
+  //   console.log(watch());
+  //   console.log(handleSubmit);
+  const onSubmit = (data) => {
+    console.log(watch("password"));
+    // console.log(data);
+  };
 
   return (
     <>
@@ -45,22 +49,63 @@ export const Signup = () => {
             <span className="inputParent">
               <label>Enter your Eamil</label>
               <input
-                {...register("lastName", {
+                {...register("email", {
                   pattern:
                     /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
                 })}
               />
-              {errors?.lastName?.type === "pattern" && (
+              {errors?.email?.type === "pattern" && (
                 <p>Pplease type a valid email</p>
               )}
             </span>
-            <span className="inputParent">
+            {/* <span className="inputParent">
               <label>Age</label>
               <input {...register("age", { min: 18, max: 99 })} />
               {errors?.age && (
                 <p>You Must be older then 18 and younger then 99 years old</p>
               )}
+            </span> */}
+            <span>
+              <label htmlFor="password">password</label>
+              <input
+                id="password"
+                {...register("password", {
+                  required: "required",
+                  minLength: {
+                    value: 5,
+                    message: "min length is 5",
+                  },
+                })}
+                type="password"
+              />
+              {errors.password && (
+                <span role="alert">{errors.password.message}</span>
+              )}
             </span>
+            <span>
+              <label htmlFor="password">Confirm password</label>
+              <input
+                id="confirmPassword"
+                {...register("confirmPassword", {
+                  required: "required",
+                  message: "Password doesn't match",
+                  validate: (value) => value === watch("password"),
+                  minLength: {
+                    vlaue: 5,
+                    // value: () => 5,
+                    // validate: (value) => value === watch("password"),
+
+                    // validate: false,
+                  },
+                })}
+                type="password"
+              />
+              {/* {errors.confirmPassword && <p>Passwords do not match.</p>} */}
+              {errors.confirmPassword && (
+                <span role="alert">Password does not mathc</span>
+              )}
+            </span>
+
             <input type="submit" />
           </div>
         </form>
